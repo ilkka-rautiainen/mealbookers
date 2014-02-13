bc2ead2e8317f9ef53150a1ef5a24fff50c4ea33
Ilkka Rautiainen <ilkka.rautiainen@yap.fi>
2014-02-13 17:00:32 +0200

    navbar improved

--- php/index.php
+++ php/index.php
@@ -15,9 +15,9 @@
 	<script type="text/javascript" src="js/filters.js"></script>
 	<script type="text/javascript" src="js/services.js"></script>
 	<script type="text/javascript" src="js/localization.js"></script>
-	<link rel="stylesheet" href="css/main.css" />
-	<link rel="stylesheet" href="css/bootstrap.min.css" />
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
 	<link rel="stylesheet" href="css/bootstrap-theme.min.css" />
+    <link rel="stylesheet" href="css/main.css" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
