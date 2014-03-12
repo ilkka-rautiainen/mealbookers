85743d08b829b1ee5846cdca0921a01dcba5084e
Ilkka Rautiainen <ilkka.rautiainen@yap.fi>
2014-03-12 11:06:55 +0200

    tuas opening hours added

--- php/app/sql_updates/update0015.sql
+++ php/app/sql_updates/update0015.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS `restaurant_opening_hours` (
   `end_weekday` tinyint(1) unsigned NOT NULL,
   `start_time` time NOT NULL,
   `end_time` time NOT NULL,
-  `type` enum('normal','lunch','breakfast') NOT NULL
+  `type` enum('normal','lunch','breakfast','alacarte') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 ALTER TABLE  `restaurant_opening_hours` ADD UNIQUE  `base_index` (  `restaurant_id` ,  `start_weekday` ,  `end_weekday` ,  `type` );
