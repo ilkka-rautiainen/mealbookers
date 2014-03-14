8d2e28419b1c210b8340d3c8047724787e7d76ef
Ilkka Rautiainen <ilkka.rautiainen@yap.fi>
2014-03-14 23:32:35 +0200

    suggestion members ui improved

--- php/js/filters.js
+++ php/js/filters.js
@@ -14,4 +14,12 @@ angular.module('Mealbookers.filters', [])
     return function(text) {
         return String(text).replace(/\%VERSION\%/mg, version);
     }
-}]);
+}])
+
+.filter('capitalize', function() {
+	return function(input) {
+		if (input)
+			input = input.toLowerCase();
+		return input.substring(0,1).toUpperCase()+input.substring(1);
+	}
+});
