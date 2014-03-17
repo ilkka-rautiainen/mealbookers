40735ea3e2abaa7e24befd452cdd7deec883b284
Ilkka Rautiainen <ilkka.rautiainen@yap.fi>
2014-03-17 22:47:22 +0200

    api route fix

--- php/api/1.0/index.php
+++ php/api/1.0/index.php
@@ -3,7 +3,7 @@ require_once '../../app/app.php';
 require_once '../../app/lib/flight/Flight.php';
 require_once 'App.php';
 require_once 'Restaurants.php';
-require_once 'User.php';
 require_once 'Groups.php';
+require_once 'User.php';
 
 Flight::start();
