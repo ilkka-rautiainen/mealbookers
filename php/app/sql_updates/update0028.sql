2d8a7b5e17459b60acf4fbe7057210973ce26579
Ilkka Rautiainen <ilkka.rautiainen@yap.fi>
2014-04-10 15:18:05 +0300

    sodexo restaurants added

---
+++ php/app/sql_updates/update0028.sql
@@ -0,0 +1,17 @@
+INSERT INTO `restaurants` (`id`, `name`, `street_address`, `location`, `link`)
+VALUES (8, 'Konetekniikka', 'Otakaari 4', GeomFromText('POINT(60.1870806 24.8267664)'), 'http://www.sodexo.fi/konetekniikka');
+
+INSERT INTO `restaurants` (`id`, `name`, `street_address`, `location`, `link`)
+VALUES (9, 'T-talo', 'Konemiehentie 2', GeomFromText('POINT(60.1867527 24.8223778)'), 'http://www.sodexo.fi/tietotekniikantalo');
+
+INSERT INTO `restaurants` (`id`, `name`, `street_address`, `location`, `link`)
+VALUES (10, 'Sähkötekniikka', 'Otakaari 5', GeomFromText('POINT(60.1897271 24.8316325)'), 'http://www.sodexo.fi/sahkotekniikka');
+
+INSERT INTO `restaurants` (`id`, `name`, `street_address`, `location`, `link`)
+VALUES (11, 'Kasper', 'Vuorimiehentie 2', GeomFromText('POINT(60.183171 24.8251114)'), 'http://www.sodexo.fi/kasper');
+
+INSERT INTO `restaurants` (`id`, `name`, `street_address`, `location`, `link`)
+VALUES (12, 'Dipoli', 'Otakaari 24', GeomFromText('POINT(60.1849873 24.8320943)'), 'http://www.sodexo.fi/dipoli');
+
+INSERT INTO `restaurants` (`id`, `name`, `street_address`, `location`, `link`)
+VALUES (13, 'Cantina', 'Otakaari 24', GeomFromText('POINT(60.1849873 24.8320943)'), 'http://www.sodexo.fi/cantina');
