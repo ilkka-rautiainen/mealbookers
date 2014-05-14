56ed571131fcd0bb2b22c1ace80ddd7377678680
Ilkka Rautiainen <ilkka.rautiainen@yap.fi>
2014-05-14 21:28:31 +0300

    t-talo opening hours added

--- php/app/import/TtaloImport.php
+++ php/app/import/TtaloImport.php
@@ -4,7 +4,6 @@ class TtaloImport extends SodexoImport implements iImport
 {
     protected $restaurant_id = 9;
     protected $sodexo_id = 142;
-    
 
     /**
      * Import and Save opening hours
@@ -14,41 +13,79 @@ class TtaloImport extends SodexoImport implements iImport
         Logger::debug(__METHOD__ . "T talo Import time");
         $source = $this->fetchURL("http://www.sodexo.fi/tietotekniikantalo");
         phpQuery::newDocument($source);
-        $children = pq("div.content:eq(15)>div:eq(2)>div>span.times");
-        /* Lunch */
-        #Logger::debug(__METHOD__ . $children->text());
-        Logger::debug(__METHOD__ . " " . sizeof($children[0]));
-        foreach ($children as $child) {
-            Logger::debug(__METHOD__ . " " . pq($child)->text());
-        }
-        /* Opening */
-        $children = pq("div.content:eq(15)>div:eq(0)>div>span.times");
-        #Logger::debug(__METHOD__ . $children->text());
-        foreach ($children as $child) {
-            Logger::debug(__METHOD__ . " " . pq($child)->text());
+
+        $rows = array();
+
+        $part_numbers = array("0", "1", "2");
+        foreach ($part_numbers as $part_number)
+        // Cafe, Breakfast, Lunch parts
+        foreach (pq(".block-sxo-opening-hours div.content > .part:eq($part_number) span") as $span) {
+            $rows[] = pq($span)->text();
         }
 
-        if (!preg_match("/Ma[\s]*\-[\s]*To\:[\s]*(([0-9]|0[0-9]|1[0-9]|2[0-3])[:.][0-5][0-9])[\s]*\-[\s]*(([0-9]|0[0-9]|1[0-9]|2[0-3])[:.][0-5][0-9])"
-            . "Pe\:[\s]*(([0-9]|0[0-9]|1[0-9]|2[0-3])[:.][0-5][0-9])[\s]*\-[\s]*(([0-9]|0[0-9]|1[0-9]|2[0-3])[:.][0-5][0-9])/", $children, $matches)) {
-            Logger::debug(__METHOD__ . " lines didn't match regex");
+        $opening_hours_string = implode("|", $rows);
 
+        if (!preg_match('/[\s]*ma[\s]*\-[\s]*to[\s]*\|'
+            . '[\s]*(([0-9]|0[0-9]|1[0-9]|2[0-3])[:.][0-5][0-9])[\s]*\-[\s]*(([0-9]|0[0-9]|1[0-9]|2[0-3])[:.][0-5][0-9])[\s]*\|'
+            . '[\s]*pe[\s]*\|'
+            . '[\s]*(([0-9]|0[0-9]|1[0-9]|2[0-3])[:.][0-5][0-9])[\s]*\-[\s]*(([0-9]|0[0-9]|1[0-9]|2[0-3])[:.][0-5][0-9])[\s]*\|'
+            . '[\s]*la[\s]*\-[\s]*su[\s]*\|'
+            . '[\s]*suljettu[\s]*\|'
+            . '[\s]*ma[\s]*\-[\s]*pe[\s]*\|'
+            . '[\s]*(([0-9]|0[0-9]|1[0-9]|2[0-3])[:.][0-5][0-9])[\s]*\-[\s]*(([0-9]|0[0-9]|1[0-9]|2[0-3])[:.][0-5][0-9])[\s]*\|'
+            . '[\s]*la[\s]*\-[\s]*su[\s]*\|'
+            . '[\s]*suljettu[\s]*\|'
+            . '[\s]*ma[\s]*\-[\s]*pe[\s]*\|'
+            . '[\s]*(([0-9]|0[0-9]|1[0-9]|2[0-3])[:.][0-5][0-9])[\s]*\-[\s]*(([0-9]|0[0-9]|1[0-9]|2[0-3])[:.][0-5][0-9])[\s]*\|'
+            . '[\s]*la[\s]*\-[\s]*su[\s]*\|'
+            . '[\s]*suljettu[\s]*/i',
+            $opening_hours_string, $matches))
+        {
+            Logger::error(__METHOD__ . " lines didn't match regex");
         }
         else {
             Logger::debug(__METHOD__ . " lines matched");
-            Logger::debug(__METHOD__ . $matches[1]);
+            $mon_thu_cafe_start = str_replace(".", ":", $matches[1]);
+            $mon_thu_cafe_end = str_replace(".", ":", $matches[3]);
+            $fri_cafe_start = str_replace(".", ":", $matches[5]);
+            $fri_cafe_end = str_replace(".", ":", $matches[7]);
+            $mon_fri_breakfast_start = str_replace(".", ":", $matches[9]);
+            $mon_fri_breakfast_end = str_replace(".", ":", $matches[11]);
+            $mon_fri_lunch_start = str_replace(".", ":", $matches[13]);
+            $mon_fri_lunch_end = str_replace(".", ":", $matches[15]);
+
+            DB::inst()->query("DELETE FROM restaurant_opening_hours WHERE restaurant_id = {$this->restaurant_id}");
+            DB::inst()->query("INSERT INTO restaurant_opening_hours (
+                    restaurant_id, start_weekday, end_weekday, start_time, end_time, type
+                ) VALUES (
+                    {$this->restaurant_id}, 0, 3, '$mon_thu_cafe_start:00', '$mon_thu_cafe_end:00', 'normal'
+                )");
+            DB::inst()->query("INSERT INTO restaurant_opening_hours (
+                    restaurant_id, start_weekday, end_weekday, start_time, end_time, type
+                ) VALUES (
+                    {$this->restaurant_id}, 4, 4, '$fri_cafe_start:00', '$fri_cafe_end:00', 'normal'
+                )");
+            DB::inst()->query("INSERT INTO restaurant_opening_hours (
+                    restaurant_id, start_weekday, end_weekday, start_time, end_time, type
+                ) VALUES (
+                    {$this->restaurant_id}, 0, 3, '$mon_thu_cafe_start:00', '$mon_thu_cafe_end:00', 'cafe'
+                )");
+            DB::inst()->query("INSERT INTO restaurant_opening_hours (
+                    restaurant_id, start_weekday, end_weekday, start_time, end_time, type
+                ) VALUES (
+                    {$this->restaurant_id}, 4, 4, '$fri_cafe_start:00', '$fri_cafe_end:00', 'cafe'
+                )");
+            DB::inst()->query("INSERT INTO restaurant_opening_hours (
+                    restaurant_id, start_weekday, end_weekday, start_time, end_time, type
+                ) VALUES (
+                    {$this->restaurant_id}, 0, 4, '$mon_fri_lunch_start:00', '$mon_fri_lunch_end:00', 'lunch'
+                )");
+            DB::inst()->query("INSERT INTO restaurant_opening_hours (
+                    restaurant_id, start_weekday, end_weekday, start_time, end_time, type
+                ) VALUES (
+                    {$this->restaurant_id}, 0, 4, '$mon_fri_breakfast_start:00', '$mon_fri_breakfast_end:00', 'breakfast'
+                )");
+            Logger::debug(__METHOD__ . " opening hours saved successfully");
         }
-
-        #Logger::debug(__METHOD__ . );
-        #content clearfix
     }
-
-    private function decreaseWithHalfHour($time)
-    {
-        return date("H:i", strtotime("1.2.2010 $time") - 1800);
-    }
-    
-
-
-
-
 }
